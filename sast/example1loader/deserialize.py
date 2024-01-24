b36e9a3160e89c78b880f9e2765b48a6b276d3e7
eurogig <eurogig@gmail.com>
2024-01-24 14:41:58 +0000

    Small fixes to loader

--- sast/example1loader/deserialize.py
+++ sast/example1loader/deserialize.py
@@ -1,8 +1,9 @@
 import yaml
+from serial import Person
 
 # class Person needs to be present in the scope
 with open('person.yml', 'r') as input_file:
     person = yaml.load(input_file, Loader = yaml.Loader)
+    print(person)
 
-print(person)
 
--- sast/example1loader/serial.py
+++ sast/example1loader/serial.py
@@ -6,15 +6,11 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        os.system("curl -O https://raw.githubusercontent.com/hlxsites/prisma-cloud-docs/main/README.md --silent")
 
     def __str__(self):
-        return f''
+        return f'{self.name} is {self.age}'
 
     def __repr__(self):
         return str(self)
 
-person = Person('Dhruv', 24)
-with open('person.yml', 'w') as output_file:
-    yaml.dump(person, output_file)
 
