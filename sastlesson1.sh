72298a1fce8c81cea574efa77b77360cb8f4bf86
eurogig <eurogig@gmail.com>
2024-01-25 09:45:00 +0000

    Examples for the lessons

--- sastlesson1.sh
+++ sastlesson1.sh
@@ -1,1 +1,1 @@
-checkov -s -d sast/example1loader/ --framework sast_python  --bc-api-key <yourkey> --repo-id cli/uniquereponame --prisma-api-url https://api3.prismacloud.io/ --external-checks-dir sast_rules
+checkov -s -d sast/example1loader/ --framework sast_python  --repo-id cli/uniquereponame --prisma-api-url https://api3.prismacloud.io/ --skip-results-upload --external-checks-dir sast_rules
