dd2a404810e7b5649fa0905d645f1ad4eab7f479
Your Name <you@example.com>
2024-01-25 17:22:34 +0100

    Small enhancements

--- sastlesson1.sh
+++ sastlesson1.sh
@@ -1,1 +1,2 @@
+#! /bin/bash
 checkov -s -d sast/example1loader/ --framework sast_python  --repo-id cli/uniquereponame --prisma-api-url https://api3.prismacloud.io/ --skip-results-upload --external-checks-dir sast_rules
--- sastlesson2.sh
+++ sastlesson2.sh
@@ -1,1 +1,2 @@
+#! /bin/bash
 checkov -s -d sast/examplewebform/ --framework sast_python  --repo-id cli/uniquereponame2 --prisma-api-url https://api3.prismacloud.io/ --skip-results-upload --external-checks-dir sast_rules
